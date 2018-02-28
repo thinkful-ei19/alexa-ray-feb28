28a12d55c8f49d2002d355a2bc192ba9e349a254
Alexa Scott <arscott4792@gmail.com>
2018-02-28 09:44:24 -0800

    updated api file

--- scripts/api.js
+++ scripts/api.js
@@ -1,9 +1,12 @@
-const api = (function() {
+'use strict'
+
+const api = (function () {
   const BASE_URL = 'https://thinkful-list-api.herokuapp.com/alexa-ray';
-  const getItems = function(callback) {
-    callback('api module works!');
+  const getItems = function (callback) {
+    $.getJSON( `${BASE_URL}/items`, callback);
+    //callback('api module works!');
   };
-    
+
   return {
     getItems
   };
