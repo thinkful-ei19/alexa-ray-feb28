23089e87d8a970fa44180828a6f681bd31660710
Alexa Scott <arscott4792@gmail.com>
2018-02-28 11:01:20 -0800

    added createItem method and tested

--- scripts/api.js
+++ scripts/api.js
@@ -1,22 +1,24 @@
-'use strict'
+'use strict';
 
 const api = (function () {
   const BASE_URL = 'https://thinkful-list-api.herokuapp.com/alexa-ray';
   const getItems = function (callback) {
-    $.getJSON( `${BASE_URL}/items`, callback);
+    $.getJSON(`${BASE_URL}/items`, callback);
     //callback('api module works!');
   };
   const createItem = function(name, callback) {
     //this data will be sent into POST request
     let newItem = JSON.stringify({name: name});
-  }
-  $.ajax({
-    url: `${BASE_URL}/items`,
-    method: 'POST',
-    constentType: 'application/json',
-    data: newItem,
-    success: callback
-});
+    $.ajax({
+      url: `${BASE_URL}/items`,
+      method: 'POST',
+      contentType: 'application/json',
+      dataType: 'json',
+      data: newItem,
+      success: callback
+    });
+  };
+
 
   return {
     getItems,
